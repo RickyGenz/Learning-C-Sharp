e7ddbe78ea6719ad64bec9b22875db63fa1f0ef4
Ricky Genz <rgenz1@gmail.com>
2016-06-24 15:07:35 -0700

    Complete Language Elements Challenge One

---
+++ Learning-C-Sharp/LanguageElementsChallengeOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageElementsChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge Objectives
+            // - Create a for loop in C#
+            // - Have the loop count to 10
+            // - Output each value as the loop counts
+            for(int count = 0; count < 11; count++)
+            {
+                Console.WriteLine(count);
+            }
+
+            // Instructor's Solution
+            for(int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
