66855776d4472de24a92cd79380d05fba7f0e417
Ricky Genz <rgenz1@gmail.com>
2016-06-24 15:15:47 -0700

    Complete Language Elements Challenge Two

---
+++ Learning-C-Sharp/LanguageElementsChallengeTwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageElementsChallengeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge Objectives
+            // - Create a function in C#
+            // - This function will not return a value
+            // - It should accept two integers
+            // - It should add the integers to form a sum
+            // - Output the sum to the console window
+            DisplaySum(11, 99); // 110
+
+            // Instructor's Solution
+            Sum(32, 45); // 77
+        }
+
+        // Custom Function to meet Challenege Objectives
+        static void DisplaySum(int one, int two)
+        {
+            Console.WriteLine(one + two);
+        }
+
+        // Instructor's Solution
+        static void Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
