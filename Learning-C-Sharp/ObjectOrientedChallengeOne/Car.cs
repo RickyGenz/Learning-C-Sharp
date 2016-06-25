1b06da4c3e40c2b8d4f188e7c1935a74180fb177
Ricky Genz <rgenz1@gmail.com>
2016-06-25 12:31:47 -0700

    Completed Object Oriented Challenge One

---
+++ Learning-C-Sharp/ObjectOrientedChallengeOne/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedChallengeOne
+{
+    // Challenge Objectives
+    // - Create a class called Car
+    // - Store make, model, and color in class
+    // - Create a Drive method to print "Driving"
+    // - Create a Stop method to print "Stopping"
+    class Car
+    {
+        // Encapsulation of Properties
+            // Private Properties
+            private string make;
+            private string model;
+            private string color;
+
+            // Public Properties for Getting
+            // and Setting Private Properties
+            public string Make
+            {
+                get { return this.make; }
+                set { this.make = value; }
+            }
+            public string Model
+            {
+                get { return this.model; }
+                set { this.model = value; }
+            }
+            public string Color
+            {
+                get { return this.color; }
+                set { this.color = value; }
+            }
+
+        // Polymorphism Methods
+            // Drive Method
+            // - Prints "Driving" to Console
+            public virtual void Drive()
+            {
+                Console.WriteLine("Driving");
+            }
+
+            // Stop Method
+            // - Prints "Stopping" to Console
+            public virtual void Stop()
+            {
+                Console.WriteLine("Stopping");
+            }
+    }
+
+    // Instructor's Solution
+    class CarIS // Car
+    {
+        public string make;
+        public string model;
+        public string color;
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping");
+        }
+    }
+}
---
+++ Learning-C-Sharp/ObjectOrientedChallengeOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge Objectives
+            // - Instantiate a Car object
+            // - Set values for the data
+            // - Call the two methods
+            Car Hybrid = new Car();
+
+            // Configure Hybrid
+            Hybrid.Make = "Toyota";
+            Hybrid.Model = "Prius";
+            Hybrid.Color = "Black";
+
+            // Check Hybrid Properties
+            Console.WriteLine("Make: " + Hybrid.Make);
+            Console.WriteLine("Model: " + Hybrid.Model);
+            Console.WriteLine("Color: " + Hybrid.Color);
+
+            // Use Hybrid
+            Hybrid.Drive();
+            Hybrid.Stop();
+
+
+            // Instructor's Solution
+            CarIS myCar = new CarIS();
+
+            myCar.make = "Chevrolet";
+            myCar.model = "Camaro";
+            myCar.color = "Black";
+
+            myCar.Drive();
+            myCar.Stop();
+        }
+    }
+}
