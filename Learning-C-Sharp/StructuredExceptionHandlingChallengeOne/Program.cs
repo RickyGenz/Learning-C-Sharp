98af9f4d22e22c11463ec8bd79d68fa320f11e39
Ricky Genz <rgenz1@gmail.com>
2016-06-25 13:49:23 -0700

    Completed Structured Exception Handling Challenge One

---
+++ Learning-C-Sharp/StructuredExceptionHandlingChallengeOne/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuredExceptionHandlingChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challenge Objectives
+            // - Use exisitng code
+            //  - int intValue = 32;
+            //  - object objValue = intValue;
+            //  - string strValue;
+            //  - strValue = (string)objValue;
+            // - Create exception handling routine
+            // - Catch the InvalidCastException
+            // - Output the exception message
+            try
+            {
+                int intValue = 32;
+                object objValue = intValue;
+                string strValue;
+
+                strValue = (string)objValue;
+            }
+            catch (InvalidCastException ice)
+            {
+                Console.WriteLine(ice.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            // Instructor's Solution
+            int intValueIS = 32;
+            object objValueIS = intValueIS;
+            string strValueIS;
+
+            try
+            {
+                strValueIS = (string)objValueIS;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
